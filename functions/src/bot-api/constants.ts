2a1776143a4581ca7574b7b9b71405ddcbe29317
Mirko Tebaldi <realtebo@users.noreply.github.com>
2018-06-29 20:57:16 +0200

    Delete constants.ts

--- functions/src/bot-api/constants.ts
+++
@@ -1,5 +0,0 @@
-
-export const BOT_TOKEN = "605738929:AAG572LSUSUzbVLB-s0ewIQ5LiYK7vTod-s";
-export const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}/`;
-export const MIRKO = 315996772;
-export const RITA  = 108691956; 
