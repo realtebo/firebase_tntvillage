2d31b5c5bd7816f3852870ef8611b1a63d7f33af
realtebo <mirkotebaldi@yahoo.it>
2018-07-11 22:47:40 +0200

    Rimosse parole chiave dalla ricerca, perché aggiunte al CSE, + aggiunti commenti

--- functions/src/helpers/search-image.ts
+++ functions/src/helpers/search-image.ts
@@ -1,14 +1,23 @@
 import * as GoogleImages from 'google-images';
 import { CUSTOM_SEARCH_ENGINE_ID, GOOGLE_API_SEARCH_KEY } from '../google-api/constants';
+import { isMaster } from 'cluster';
 
 /**
  * Dato un titolo, più pulito possibile, cerco una immagine da allegare al messaggio
  * 
+ * Uso il mio cse: https://cse.google.com/cse/setup/basic?cx=009127734400381924296:ck3vf-rvnka
  */
 export const searchImage = async (title : string) : Promise<string> => {
     
     const client = new GoogleImages(CUSTOM_SEARCH_ENGINE_ID, GOOGLE_API_SEARCH_KEY);
     
-    const images = await client.search(`"${title}" "serie tv"`, {size: 'medium'})
+    // Ho tolto serie tv perché posso inserirle come Parole chiave nel CSE
+    const images = await client.search(`"${title}"`, {size: 'medium'});
+
+    // Fix: ovviamente NON è detto che tutti i tioli diano immagini
+    if (typeof images[0] === "undefined" ) {
+        console.log ("La ricerca di " + title + " ha prodotto: ", images);
+        return "";
+    }
     return images[0].url;
 };
