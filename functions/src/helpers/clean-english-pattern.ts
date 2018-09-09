fbfedddcc82c95da419128f5ff6a97fab3d111e6
realtebo <mirkotebaldi@yahoo.it>
2018-09-09 21:52:58 +0200

    Fix contro errori dovuti a stringhe null

--- functions/src/helpers/clean-english-pattern.ts
+++ functions/src/helpers/clean-english-pattern.ts
@@ -1,6 +1,9 @@
 import { cleanString } from './clean-string';
 
 export const cleanTechData = ( string_to_clean : string ) : string => {
+    
+    if (string_to_clean === null) return null;
+
     let out : string = cleanString(string_to_clean);
     out = string_to_clean.replace(/\./g, '');
     return out.trim().toUpperCase();
--- functions/src/helpers/clean-string.ts
+++ functions/src/helpers/clean-string.ts
@@ -1,3 +1,4 @@
 export const cleanString = ( a_string : string ) : string => {
+    if (a_string === null) return null;
     return a_string.trim().toUpperCase();
 }
