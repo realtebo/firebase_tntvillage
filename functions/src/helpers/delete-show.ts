6d408d93526599d1aedb614305f69600f93a21b6
realtebo <mirkotebaldi@yahoo.it>
2018-09-22 22:12:21 +0200

    Fix per evitare errori se la serie tv che si cerca di cancellare NON è più in db

--- functions/src/helpers/delete-show.ts
+++ functions/src/helpers/delete-show.ts
@@ -1,8 +1,6 @@
 import { json_fmt} from '../objects/result-row';
 import { get_snap_from_hash } from '../helpers/get-snap-from-hash';
 import { db } from '../app-helpers';
-import { sendTo } from '../bot-api/send-to';
-import { MIRKO } from '../bot-api/constants';
 import { editMessage } from '../bot-api/edit-message';
 import { nowAsString } from '../helpers/now-as-string';
 import { cleanString } from '../helpers/clean-string';
@@ -12,19 +10,23 @@ export const deleteShow = async (hash: string, chat_id: number, message_id: numb
     try {
         const snap : json_fmt = await get_snap_from_hash(hash);
 
-        const title = cleanString(snap.title);
+        if (typeof snap.title !== 'undefined') {
 
-        // Verifica alternativa, usando il nuovo sistema ad albero
-        await db.ref(`tv_show/${title}`).update({ 
-            'banned'        : true,
-            'banned_since'  : nowAsString()
-        });
-        
-        await sendTo(MIRKO, "Questo show è stato bannato\n" + title);
+            const title = cleanString(snap.title);
 
-        await editMessage(chat_id, message_id, `La serie TV ${title} verrà ignorata`);
+            // Verifica alternativa, usando il nuovo sistema ad albero
+            await db.ref(`tv_show/${title}`).update({ 
+                'banned'        : true,
+                'banned_since'  : nowAsString()
+            });
+
+            await editMessage(chat_id, message_id, `La serie TV ${title} verrà ignorata`);
+        } else {
+            await editMessage(chat_id, message_id, `C'è stato un errore, sarà possibile eliminare la serie la prossima volta`);
+        }
 
     } catch ( error ) {
         console.warn ( `deleteShow (hash '${hash}', chat_id '${chat_id}') catched error:` + error);
     }
+    
 }
