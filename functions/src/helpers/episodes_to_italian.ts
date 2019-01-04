e412a379d27474f6e20700a7a175d626dc4e86b2
realtebo <mirkotebaldi@yahoo.it>
2019-01-04 12:45:48 +0100

    Migliorate le descrizioni italiane

--- functions/src/helpers/episodes_to_italian.ts
+++ functions/src/helpers/episodes_to_italian.ts
@@ -10,7 +10,7 @@ export const episodesToItalian = (episodes: string): ItalianEpisodeDescriptor =>
      if (!episodes || typeof episodes === "undefined" || episodes === null) {
           return <ItalianEpisodeDescriptor>{
                season_it: "Stagione non precisata",
-               episode_it: "Episodi non precisata"
+               episode_it: "Episodi non precisati"
           };
      }
 
@@ -28,16 +28,25 @@ export const episodesToItalian = (episodes: string): ItalianEpisodeDescriptor =>
 
      let season_it: string = "";
      if (matches[3]) {
-          season_it += `Stagioni da ${matches[2]} a ${matches[4]}`;
+          if (matches[4] === (matches[2] + 1)) {
+               season_it += `Stagioni ${matches[2]} e ${matches[4]}`;
+          } else {
+               season_it += `Stagioni da ${matches[2]} a ${matches[4]}`;
+          }
      } else {
           season_it += `Stagione ${matches[2]}`;
      }
 
      let episode_it: string = "";
      if (matches[6]) {
-          episode_it += `Episodi da ${matches[5]} a ${matches[8]}`;
+          if (matches[8] === (matches[5] + 1)) {
+               episode_it += `episodi ${matches[5]} e ${matches[8]}`;
+          } else {
+               episode_it += `episodi da ${matches[5]} a ${matches[8]}`;
+          }
+
      } else {
-          episode_it += `Episodio ${matches[5]}`;
+          episode_it += `episodio ${matches[5]}`;
      }
 
      return <ItalianEpisodeDescriptor>{ episode_it, season_it };
