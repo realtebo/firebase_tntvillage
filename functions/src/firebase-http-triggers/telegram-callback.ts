35eb19ce5401d3830729bcb79fe6a67e962e77e8
realtebo <mirkotebaldi@yahoo.it>
2019-07-11 15:29:09 +0200

    Aggiornato lodash, con fix splitByEquals

--- functions/src/firebase-http-triggers/telegram-callback.ts
+++ functions/src/firebase-http-triggers/telegram-callback.ts
@@ -1,92 +1,131 @@
-import * as _ from 'lodash';
-import * as functions from 'firebase-functions';
-import * as util from 'util';
+import * as _ from "lodash";
+import * as functions from "firebase-functions";
+import * as util from "util";
 
-import { sendTo } from '../bot-api/send-to';
-import { MIRKO } from '../bot-api/constants';
-import { refresh } from '../helpers/refresh';
-import { saveEnglishPattern } from '../helpers/save-english-pattern';
-import { deleteMessageFromChat, DeleteResult } from '../helpers/delete-message';
-import { deleteShow } from '../helpers/delete-Show';
+import { sendTo } from "../bot-api/send-to";
+import { MIRKO } from "../bot-api/constants";
+import { refresh } from "../helpers/refresh";
+import { saveEnglishPattern } from "../helpers/save-english-pattern";
+import { deleteMessageFromChat, DeleteResult } from "../helpers/delete-message";
+import { deleteShow } from "../helpers/delete-Show";
 
 // Reagisce ai messaggi inviati dagli utenti umani
 const handleDirectMessage = async (message): Promise<void> => {
+  const { text, from } = message;
+  const { id, first_name, last_name } = from;
 
-    const { text, from } = message;
-    const { id, first_name, last_name } = from;
+  switch (text) {
+    case "/refresh": {
+      // Lo accetto solo da Mirko
+      if (id !== MIRKO) {
+        await sendTo(
+          MIRKO,
+          "Mittente non approvato per questo comando: " + JSON.stringify(from)
+        );
+        await sendTo(
+          id,
+          "Spiacente " +
+            first_name +
+            " " +
+            last_name +
+            ", solo alcuni utenti posso eseguire questo comando"
+        );
+        return;
+      }
 
-    switch (text) {
-        case '/refresh': {
-            // Lo accetto solo da Mirko
-            if (id !== MIRKO) {
-                await sendTo(MIRKO, "Mittente non approvato per questo comando: " + JSON.stringify(from));
-                await sendTo(id, "Spiacente " + first_name + " " + last_name + ", solo alcuni utenti posso eseguire questo comando");
-                return;
-            }
-
-            await sendTo(MIRKO, "Ricevuto comando di refresh Da Mirko");
-            const result: boolean = await refresh();
-            await sendTo(MIRKO, "Refresh avviato");
-            if (!result) {
-                console.warn("handleDirectMessage - c'è stato un errore nel refresh");
-            }
-            break;
-        }
-        case "/start":
-        default: {
-            await sendTo(MIRKO, "Comando sconosciuto: " + text + " da " + first_name + " " + last_name);
-            await sendTo(id, "Spiacente " + first_name + " " + last_name + ", questo è un bot privato!");
-        }
+      await sendTo(MIRKO, "Ricevuto comando di refresh Da Mirko");
+      const result: boolean = await refresh();
+      await sendTo(MIRKO, "Refresh avviato");
+      if (!result) {
+        console.warn("handleDirectMessage - c'è stato un errore nel refresh");
+      }
+      break;
     }
-}
+    case "/start":
+    default: {
+      await sendTo(
+        MIRKO,
+        "Comando sconosciuto: " + text + " da " + first_name + " " + last_name
+      );
+      await sendTo(
+        id,
+        "Spiacente " +
+          first_name +
+          " " +
+          last_name +
+          ", questo è un bot privato!"
+      );
+    }
+  }
+};
 
 // Reagisce ai pulsanti di azione
 
-export const telegram_callback = async (req: functions.Request, res: functions.Response): Promise<void> => {
-    const body = req.body;
-
-    // Messaggio diretto
-    if (body.message) {
-
-        await handleDirectMessage(body.message);
-        res.send("ok");
-        return;
-    }
-
-    // Messaggio diretto
-    if (body.callback_query) {
-
-        const query = _.chain(body.callback_query.data)
-            .split('&')                         // ["a=b454","c=dhjjh","f=g6hksdfjlksd"]
-            .map(_.partial(_.split, _, '=', 2)) // [["a","b454"],["c","dhjjh"],["f","g6hksdfjlksd"]]
-            .fromPairs()                        // {"a":"b454","c":"dhjjh","f":"g6hksdfjlksd"}
-            .value();
-
-        const message_id: number = <number>body.callback_query.message.message_id;
-        const chat_id: number = <number>body.callback_query.message.chat.id;
-        const from_id: number = <number>body.callback_query.from.id;
-
-        switch (query.command) {
-            case 'is_english':
-                await saveEnglishPattern(<string>query.hash, from_id, chat_id, message_id);
-                break;
-            case 'delete_message':
-                const result: DeleteResult = await deleteMessageFromChat(chat_id, message_id);
-                if (result.success === false) {
-                    await sendTo(from_id, "Messaggio non cancellabile, forse è troppo vecchio\n");
-                }
-                break;
-            case 'delete_show':
-                await deleteShow(<string>query.hash, chat_id, message_id);
-                break;
-            default:
-                await sendTo(MIRKO, "Callback query non implementata\n" + util.inspect(query));
-        }
-
-        res.send("ok");
-        return;
-    }
-
-    await sendTo(MIRKO, "Messaggio sconosciuto: " + JSON.stringify(req.body));
-    res.status(500).send("Messaggio sconosciuto");
+const splitByEquals = (string): Array<string> =>{
+    return _.split(string, "=", 2);
 }
+
+export const telegram_callback = async (
+  req: functions.Request,
+  res: functions.Response
+): Promise<void> => {
+  const body = req.body;
+
+  // Messaggio diretto
+  if (body.message) {
+    await handleDirectMessage(body.message);
+    res.send("ok");
+    return;
+  }
+
+  // Messaggio diretto
+  if (body.callback_query) {
+    
+    const query = _.chain(body.callback_query.data)
+      .split("&") // ["a=b454","c=dhjjh","f=g6hksdfjlksd"]
+      .map(splitByEquals) // [["a","b454"],["c","dhjjh"],["f","g6hksdfjlksd"]]
+      .fromPairs() // {"a":"b454","c":"dhjjh","f":"g6hksdfjlksd"}
+      .value();
+
+    const message_id: number = <number>body.callback_query.message.message_id;
+    const chat_id: number = <number>body.callback_query.message.chat.id;
+    const from_id: number = <number>body.callback_query.from.id;
+
+    switch (query.command) {
+      case "is_english":
+        await saveEnglishPattern(
+          <string>query.hash,
+          from_id,
+          chat_id,
+          message_id
+        );
+        break;
+      case "delete_message":
+        const result: DeleteResult = await deleteMessageFromChat(
+          chat_id,
+          message_id
+        );
+        if (result.success === false) {
+          await sendTo(
+            from_id,
+            "Messaggio non cancellabile, forse è troppo vecchio\n"
+          );
+        }
+        break;
+      case "delete_show":
+        await deleteShow(<string>query.hash, chat_id, message_id);
+        break;
+      default:
+        await sendTo(
+          MIRKO,
+          "Callback query non implementata\n" + util.inspect(query)
+        );
+    }
+
+    res.send("ok");
+    return;
+  }
+
+  await sendTo(MIRKO, "Messaggio sconosciuto: " + JSON.stringify(req.body));
+  res.status(500).send("Messaggio sconosciuto");
+};
